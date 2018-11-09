fbf5de0e4bb62d9b5bd30a25ac86580e85bf2b29
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-11-09 13:49:25 +0100

    Add `alloc_layout_extra` feature.
    This feature became required after recent nightly changes.

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 #![no_std]
 #![cfg_attr(
     not(feature = "disable"),
-    feature(alloc, dropck_eyepatch, allocator_api, fused, ptr_internals, try_reserve, nonnull_cast)
+    feature(alloc, dropck_eyepatch, allocator_api, fused, ptr_internals, try_reserve, nonnull_cast, alloc_layout_extra)
 )]
 
 #[cfg(not(feature = "disable"))]
